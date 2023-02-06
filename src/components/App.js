1c301755e6024a45aa48e0383be6f93ac672e2cc
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-06 13:57:40 -0500

    3 out of 4 deliverables are done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => {
+          // setting state in the useEffect callback
+        setImages(data.message);
+      });
+  }, []);
+
+  return (
+    <div>
+        <img src={images} alt="A Random Dog"/>
+    </div>
+  );
+}
+
+export default App;
